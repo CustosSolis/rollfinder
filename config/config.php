80c4eaa45f1c2356aa26179ffe3c68c450d95960
darkelement1987 <jgernaat1987@gmail.com>
2021-06-22 19:38:10 +0200

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -1,6 +1,5 @@
 <?php
-$title = "D2Rollfinder";
+$title = "Rollfinder.net";
 $spreadsheet = "https://docs.google.com/spreadsheets/d/1uZ_3QrltU2YIV5FCnzJgufB0Uti7yiXj1kaFpaiuFwk/edit#gid=";
-$rollsurl = "https://raw.githubusercontent.com/dcaslin/d2-checklist/f02374a0ce6b50576cc7936b326b419d8bb76889/src/assets/panda-godrolls.min.json";
 $apiKey = "";
 ?>
